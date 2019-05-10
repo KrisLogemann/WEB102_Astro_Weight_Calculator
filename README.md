8fec191b09cc9fb8016481c6aa60ce2d5dd58b3b
Kris Logemann <krisrlogemann@gmail.com>
2019-05-10 14:07:01 -0700

    adds initial project files

---
+++ README.md
@@ -0,0 +1,8 @@
+//Astroweight-Calculator Version 102. Created on May 10, 2019 by Kris Logemann.
+
+Hey everybody, thanks for checking out my code in my Github.
+
+This webapp is my first Javascript project for SDCS in the Fullstack Remote Program, WEB-102 section. What you see here is an "Astro-Weight-Calculator". It calculates how much you would weigh on any given planet in our solar system. Upon loading the app, the user inputs their weight, selects which planet they would like to know how much they would weigh on, and hits calculate. The code will take the gravity value of whatever planet the user chooses, multiplies it by the weight of the user, and as simple as that they know how much they would weigh on any other planet in our solar system.
+
+Used html and javascript
+
--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 // Write your JavaScript code here!
+var choosePlanet = document.getElementById("planets");
+
+var planets = [
+  ["Pluto", 0.06],
+  ["Neptune", 1.148],
+  ["Uranus", 0.917],
+  ["Saturn", 1.139],
+  ["Jupiter", 2.64],
+  ["Mars", 0.3895],
+  ["Moon", 0.1655],
+  ["Earth", 1],
+  ["Venus", 0.9032],
+  ["Mercury", 0.377],
+  ["Sun", 27.9]
+];
+
+for (var i=0; i<planets.length; i++ ){
+  choosePlanet.options.add(new Option(planets[i][0]));
+}
+function calculateWeight(weight, planetName) {
+  for (var i = 0; i < planets.length; i++) {
+    if (planetName == planets[i][0]) {
+      var newGravity = planets[i][1];
+    }
+  }
+  console.log ("this is weight"+weight);
+  console.log ("this is newGravity"+newGravity);
+  return weight * newGravity;
+}
+
+function handleClickEvent() {
+  var weight = document.getElementById("user-weight").value;
+  var planetName = document.getElementById("planets").value;
+  var result = calculateWeight(weight, planetName);
+  document.getElementById("output").innerHTML = "If you were on " + planetName + "," + " you would weigh " + result + "lbs!";
+}
